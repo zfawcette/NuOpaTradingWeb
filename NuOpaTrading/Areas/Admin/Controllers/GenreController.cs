4edaa91167d92326ca82d9d9b256aa95226b4bba
Zach Fawcett'e <zgf83810@ucmo.edu>
2022-06-07 16:01:58 -0500

    Added Genre Model & Controller + Updated Navbar Links

---
+++ NuOpaTrading/Areas/Admin/Controllers/GenreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NuOpaTrading.Areas.Admin.Controllers
+{
+    public class GenreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
